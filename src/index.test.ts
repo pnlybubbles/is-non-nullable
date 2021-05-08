e64faedbe52c3c3d21167bc3f4165415c115a236
pnlybubbles <pnlybubbles@gmail.com>
2021-05-08 19:56:15 +0900

    test: add tests

---
+++ src/index.test.ts
@@ -0,0 +1,20 @@
+import isNonNullable from './index'
+
+test('null is not non-nullable', () => {
+  expect(isNonNullable(null)).toBe(false)
+})
+
+test('undefined is not non-nullable', () => {
+  expect(isNonNullable(undefined)).toBe(false)
+})
+
+test('others are non-nullable', () => {
+  expect(isNonNullable(0)).toBe(true)
+  expect(isNonNullable(1)).toBe(true)
+  expect(isNonNullable('')).toBe(true)
+  expect(isNonNullable('string')).toBe(true)
+  expect(isNonNullable(true)).toBe(true)
+  expect(isNonNullable(false)).toBe(true)
+  expect(isNonNullable({})).toBe(true)
+  expect(isNonNullable([])).toBe(true)
+})
